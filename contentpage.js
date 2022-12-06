d2b24b5ab3b708c3a748cb592b4500a90d30c8ad
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-12-05 20:13:26 -0600

    test

--- contentpage.js
+++ contentpage.js
@@ -1,3 +1,29 @@
+if (localStorage.getItem("close-alert") === null) {
+const bootstrapjs = document.createElement("script");
+bootstrapjs.setAttribute(
+  "src",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+);
+const bootstrapcss = document.createElement("script");
+bootstrapcss.setAttribute(
+  "src",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+);
+var alertdiv = document.createElement('div')
+alertdiv.setAttribute('class', 'sticky-top')
+alertdiv.innerHTML = `
+
+<div class="alert alert-primary d-flex align-items-center alert-dismissible fade show" role="alert">
+<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg>
+<div>
+    An example alert with an icon<a href="/settings" class="alert-link">settings</a>
+  </div>
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`
+
+}
 let url = window.location.pathname; //get path
 let host = window.location.host
 /*let urlsub = "";
@@ -129,6 +155,7 @@ const iFullscreen = () => {
   reqFs(elem);
 };
 
+document.body.append(bootstrapjs, bootstrapcss);
 //Canvas fullscreen
 const cFullscreen = () => {
   const elem = document.getElementById("canvas");
