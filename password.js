82daee5cfe9a828c791fd4730db1de515414a2bf
makeawish-kid <122392699+makeawish-kid@users.noreply.github.com>
2023-01-12 19:43:07 -0600

    Create password.js

---
+++ password.js
@@ -0,0 +1,54 @@
+import $ from "https://cdn.skypack.dev/jquery@3.6.3";
+function doAuthentication(pass, theHTML, thePrompt) { //do authentication}
+  
+	if (pass != 'password1234') {
+		var theHTML2 = "<p> Please try again.</p>";
+    
+    thePrompt.document.body.innerHTML = theHTML2;
+    
+	} 
+  else {
+    
+    var theHTML3 = "<p> Success</p>";
+    thePrompt.document.body.innerHTML = theHTML3;
+		var url = "https://makeawish-kid.github.io/good"
+		var myWindow = window.open("", "_blank");
+		myWindow.document.write('<title>Google</title><link rel=\"icon\" type=\"image/x-icon\" href=\"https://www.google.com/favicon.ico\">')
+		myWindow.document.write("<iframe onload=\"\" id='iframe' width=\"100%\" height=\"100%\" style=\"border:none;\"></iframe>");
+		myWindow.document.write(`<script>setTimeout(function() {document.getElementById('iframe').src = url}, 500)</script>`)
+		myWindow.document.write('<style>body { margin:0;}</style>')
+		myWindow.document.write("<script>function home(){document.getElementById('iframe').src = url}</script>")
+		myWindow.document.close();
+		myWindow.stop();
+		window.location.replace('https://google.com');
+	}
+  
+}
+
+$(function() {
+ 
+
+	var theButton = $('#prompt')
+  
+	theButton.click(function() {
+    
+    
+		var thePrompt = window.open("", "Password", "name=yes,top=500,left=500,width=400,height=400");
+    
+  var theHTML = "";   
+		
+		theHTML += "<p> Please authenticate yourself.</p>";
+		theHTML += "<br/>";
+		theHTML += "Password: <input type='text' id='thePass' placeholder='Enter Password...'/>";
+		theHTML += "<br />";
+		theHTML += "<input type='button' value='OK' id='authOK'/>";
+    theHTML += '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" crossorigin="anonymous" referrerpolicy="no-referrer"></script>';
+		$(thePrompt.document.body).html(theHTML);
+    alert($( 'thePrompt > #thePass' ).val())
+   var thePass = ""
+		thePass = $( 'thePrompt > #thePass'  ).val();
+		$("thePrompt > #authOK").click(doAuthentication(thePass, theHTML, thePrompt)); 
+	});
+  
+})
+
